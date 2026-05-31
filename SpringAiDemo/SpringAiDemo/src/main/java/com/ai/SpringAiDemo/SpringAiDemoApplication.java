82d60e2828e9bed2b89e3ba36255ab2aa4eea7d9
adityasawant123 <adityasawant132@gmail.com>
2026-05-31 23:01:00 +0530

    GenSphere-AI-Multi-Modal-Content-Generation-Platform

---
+++ SpringAiDemo/SpringAiDemo/src/main/java/com/ai/SpringAiDemo/SpringAiDemoApplication.java
@@ -0,0 +1,15 @@
+package com.ai.SpringAiDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiDemoApplication.class, args);
+	}
+
+}
+
+//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
